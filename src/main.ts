75d7dafd82f4f615052a0d826c33e31bba5aaad2
Advait Shinde <advait.shinde@gmail.com>
2024-01-23 16:49:21 -0800

    Render terminal node colors.

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,28 @@
-import "./style.css";
-
 import * as Plot from "@observablehq/plot";
 
 type MctsNode = {
   name: string;
+  value: number;
   children: MctsNode[];
 };
 
 function generateTree(
   maxDepth: number,
+  terminalValueFn: (depth: number) => number,
   curDepth: number = 0,
   nodeName: string = "",
   fanout: number = 2,
 ): MctsNode {
   if (curDepth >= maxDepth) {
-    return { name: nodeName, children: [] };
+    return { name: nodeName, value: terminalValueFn(curDepth), children: [] };
   }
 
-  const ret: MctsNode = { name: nodeName, children: [] };
+  const ret: MctsNode = { name: nodeName, value: 0, children: [] };
   for (let i = 0; i < fanout; i++) {
     let childName = nodeName ? `${nodeName}.${i}` : `${i}`;
-    ret.children.push(generateTree(maxDepth, curDepth + 1, childName, fanout));
+    ret.children.push(
+      generateTree(maxDepth, terminalValueFn, curDepth + 1, childName, fanout),
+    );
   }
   return ret;
 }
@@ -34,16 +36,23 @@ function flattenTree(tree: MctsNode): MctsNode[] {
 
 function renderPlot(tree: MctsNode): (SVGElement | HTMLElement) & Plot.Plot {
   return Plot.plot({
-    height: 1200,
-    width: 500,
+    height: 1400,
+    width: 400,
     margin: 10,
     axis: null,
+    color: {
+      scheme: "RdYlGn",
+    },
     marks: [
       Plot.tree(flattenTree(tree), {
         path: "name",
         delimiter: ".",
-        fill: "gray",
-        stroke: () => Math.random(),
+        r: 4,
+        symbol: (d: MctsNode) => (d.children.length ? "circle" : "square"),
+        fill: (d) => d.value,
+        stroke: "gray",
+        strokeWidth: 3,
+        strokeLinecap: "square",
         text: "null",
       }),
     ],
@@ -54,6 +63,6 @@ function renderPlot(tree: MctsNode): (SVGElement | HTMLElement) & Plot.Plot {
 }
 
 const div = document.querySelector<HTMLDivElement>("#app")!;
-const tree = generateTree(7);
+const tree = generateTree(7, () => Math.random() * 2 - 1);
 const plot = renderPlot(tree);
 div.append(plot);
